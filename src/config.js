d9137cf2d6cca6fd628db89d1f1cdf3fe872f8d4
Tung <tung@hesper.io>
2019-09-01 23:08:46 -0400

    reset apikey

--- src/config.js
+++ src/config.js
@@ -27,7 +27,7 @@ setInterval(() => {
   console.log(arrayKeys[0]);
 }, 1000 * 60 * 60);
 
-const F2FAPIKEY = arrayKeys[2];
+const F2FAPIKEY = arrayKeys[0];
 
 export default class config {
   // Food2fork
